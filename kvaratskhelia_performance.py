cd6b678eed59aab280a8fe5e4de5229a7dba1de4
Vinicius Melo <viniciumelo@hotmail.com>
2026-07-02 13:57:53 -0300

    - test performance Kvara

--- kvaratskhelia_performance.py
+++ kvaratskhelia_performance.py
@@ -38,3 +38,29 @@ fig.add_trace(go.Scatterpolar(
     name='Khvicha Kvaratskhelia'
 ))
 
+# 3. Estilização do Layout
+fig.update_layout(
+    title=dict(
+        text="Análise de Desempenho Médio - Khvicha Kvaratskhelia",
+        font=dict(size=22, color='white'),
+        x=0.5,
+        y=0.95
+    ),
+    polar=dict(
+        radialaxis=dict(
+            visible=True,
+            range=[0, 100],
+            gridcolor="rgba(255, 255, 255, 0.2)",
+            tickfont=dict(color="rgba(255, 255, 255, 0.7)")
+        ),
+        angularaxis=dict(
+            gridcolor="rgba(255, 255, 255, 0.3)",
+            tickfont=dict(size=12, color='white')
+        ),
+        bgcolor='rgb(20, 24, 35)' # Fundo escuro para destacar os dados
+    ),
+    paper_bgcolor='rgb(20, 24, 35)',
+    showlegend=False,
+    width=700,
+    height=600
+)
