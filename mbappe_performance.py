8878cf88f91e5b5cb75325dca6f3313d42447566
Vinicius Melo <viniciumelo@hotmail.com>
2026-07-07 16:14:30 -0300

    - test performance Mbappe

--- mbappe_performance.py
+++ mbappe_performance.py
@@ -1,2 +1,10 @@
 import plotly.graph_objects as go
 
+# 1. Definição das métricas e os valores médios de desempenho do Mbappé
+# Os valores do gráfico estão em escala de 0 a 100 para manter a proporção visual,
+# enquanto os valores reais de scouts profissionais aparecem no detalhamento (hover).
+metricas = [
+    'Posse de Bola<br>(Condução e Dribles)', 
+    'Velocidade Máxima<br>(Aceleração/Sprints)', 
+    'Chances de Gol<br>(Finalizações e xG)'
+]
