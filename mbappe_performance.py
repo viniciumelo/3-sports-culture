f7d571a267c7fb292daf4220938928382334dda4
Vinicius Melo <viniciumelo@hotmail.com>
2026-07-08 08:46:40 -0300

    - test performance Mbappe

--- mbappe_performance.py
+++ mbappe_performance.py
@@ -34,4 +34,7 @@ fig.add_trace(go.Scatterpolar(
     fill='toself',
     fillcolor='rgba(220, 20, 60, 0.3)',  # Tom avermelhado escuro para contraste
     line=dict(color='crimson', width=2),
+    text=valores_reais_fechados,
+    hovertemplate="<b>%{theta}</b><br>Nível Geral: %{r}/100<br>Dado Real: %{text}<extra></extra>",
+    name='Kylian Mbappé'
 ))
