dc6976f78a113ff4efe9ec7120e8ae2e919a8cbc
Vinicius Melo <viniciumelo@hotmail.com>
2026-07-14 18:04:18 -0300

    - test performance Messi

--- messi_performance.py
+++ messi_performance.py
@@ -5,5 +5,6 @@ import plotly.graph_objects as go
 # enquanto os valores reais de scouts profissionais aparecem no detalhamento (hover).
 metricas = [
     'Posse de Bola<br>(Dribles, Pivô e Visão)', 
- 
+    'Velocidade Máxima<br>(Aceleração Curta)', 
+    'Chances de Gol<br>(Gols, Assistências e Key Passes)'
 ]
