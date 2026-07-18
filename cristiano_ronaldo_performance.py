34eae583b34eb5e6a32cde490998a387be4e2c15
Vinicius Melo <viniciumelo@hotmail.com>
2026-07-17 21:18:43 -0300

    - test performance Cr7

--- cristiano_ronaldo_performance.py
+++ cristiano_ronaldo_performance.py
@@ -1,2 +1,9 @@
 import plotly.graph_objects as go
 
+# 1. Definição das métricas e os valores médios de desempenho do Cristiano Ronaldo
+# Os valores do gráfico estão em escala de 0 a 100 para manter a proporção visual ideal,
+# enquanto os valores reais de scouts profissionais aparecem no detalhamento (hover).
+metricas = [
+    'Posse de Bola<br>(Toques na Área/Retenção)', 
+    
+]
