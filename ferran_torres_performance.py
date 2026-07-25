15bd08d48b8a9e673f68cf5b1f79a92c4c9b06be
Vinicius Melo <viniciumelo@hotmail.com>
2026-07-24 21:52:33 -0300

    - test performance Fernan Torres

--- ferran_torres_performance.py
+++ ferran_torres_performance.py
@@ -27,3 +27,14 @@ valores_reais_fechados = valores_reais + [valores_reais[0]]
 
 # 2. Construção do Gráfico de Radar Interativo
 fig = go.Figure()
+
+fig.add_trace(go.Scatterpolar(
+    r=valores_grafico_fechados,
+    theta=metricas_fechadas,
+    fill='toself',
+    fillcolor='rgba(0, 102, 204, 0.3)',  # Tom azul royal para destaque analítico
+    line=dict(color='royalblue', width=2),
+    text=valores_reais_fechados,
+    hovertemplate="<b>%{theta}</b><br>Nível Geral: %{r}/100<br>Dado Real: %{text}<extra></extra>",
+    name='Ferran Torres'
+))
