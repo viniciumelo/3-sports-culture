62bdbeed4248a682a6ea84852037587bc1310568
Vinicius Melo <viniciumelo@hotmail.com>
2026-07-31 23:05:52 -0300

    - test performance Palmer

--- palmer_performance.py
+++ palmer_performance.py
@@ -5,4 +5,5 @@ import plotly.graph_objects as go
 # enquanto os valores reais de scouts profissionais aparecem no detalhamento (hover).
 metricas = [
     'Posse de Bola<br>(Passe, Retenção e Controle)', 
+    'Velocidade Máxima<br>(Aceleração e Deslocamento)', 
 ]
