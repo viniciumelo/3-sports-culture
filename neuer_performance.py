eb8e7fb297a3e9eb85ccd29c55c8767bac5dbca5
Vinicius Melo <viniciumelo@hotmail.com>
2026-08-03 16:04:08 -0300

    - test performance Neuer

--- neuer_performance.py
+++ neuer_performance.py
@@ -2,4 +2,7 @@ mport plotly.graph_objects as go
 
 # 1. Definição das métricas e os valores médios de desempenho do Manuel Neuer
 # Os valores do gráfico estão em escala de 0 a 100 para manter a proporção visual ideal,
-# enquanto os dados de scout especializado aparece
+# enquanto os dados de scout especializado aparecem no detalhamento (hover).
+metricas = [
+    'Posse de Bola<br>(Jogo com os Pés e Saída)', 
+]
