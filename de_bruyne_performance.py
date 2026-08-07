a0c53fb180f5f25b6aafa93d0617b7840787cc7d
Vinicius Melo <viniciumelo@hotmail.com>
2026-08-07 17:53:31 -0300

    - test performance Bruyne

---
+++ de_bruyne_performance.py
@@ -0,0 +1,8 @@
+import plotly.graph_objects as go
+
+# 1. Definição das métricas e os valores médios de desempenho do Kevin De Bruyne
+# Os valores do gráfico estão em escala de 0 a 100 para manter a proporção visual ideal,
+# enquanto os dados de scout especializado aparecem no detalhamento (hover).
+metricas = [
+    'Posse de Bola<br>(Retenção e Distribuição)', 
+]
