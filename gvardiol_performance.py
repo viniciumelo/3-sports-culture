e686de5f6e7a999473ae3be3c83464bea395f62f
Vinicius Melo <viniciumelo@hotmail.com>
2026-08-11 16:30:04 -0300

    - test performance Gvardiol

--- gvardiol_performance.py
+++ gvardiol_performance.py
@@ -5,4 +5,5 @@ import plotly.graph_objects as go
 # enquanto os dados de scout especializado aparecem no detalhamento (hover).
 metricas = [
     'Posse de Bola<br>(Saída de Bola e Condução)', 
+    'Velocidade Máxima<br>(Aceleração e Cobertura)', 
 ]
