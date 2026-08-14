76da224e5282cc8d3b38cbabdac6700f7060646b
Vinicius Melo <viniciumelo@hotmail.com>
2026-08-14 09:31:15 -0300

    - test performance Gvardiol

--- gvardiol_performance.py
+++ gvardiol_performance.py
@@ -32,4 +32,9 @@ fig.add_trace(go.Scatterpolar(
     r=valores_grafico_fechados,
     theta=metricas_fechadas,
     fill='toself',
+    fillcolor='rgba(70, 130, 180, 0.3)',  # Tom azul aço (Steel Blue)
+    line=dict(color='steelblue', width=2),
+    text=valores_reais_fechados,
+    hovertemplate="<b>%{theta}</b><br>Nível Geral: %{r}/100<br>Dado Real: %{text}<extra></extra>",
+    name='Joško Gvardiol'
 ))
