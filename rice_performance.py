297043342dfe62c339a2743f9079c2304ea60c2f
Vinicius Melo <viniciumelo@hotmail.com>
2026-08-17 14:47:46 -0300

    - test performance Rice

--- rice_performance.py
+++ rice_performance.py
@@ -47,4 +47,17 @@ fig.update_layout(
         x=0.5,
         y=0.95
     ),
+    polar=dict(
+        radialaxis=dict(
+            visible=True,
+            range=[0, 100],
+            gridcolor="rgba(255, 255, 255, 0.2)",
+            tickfont=dict(color="rgba(255, 255, 255, 0.7)")
+        ),
+        angularaxis=dict(
+            gridcolor="rgba(255, 255, 255, 0.3)",
+            tickfont=dict(size=12, color='white')
+        ),
+        bgcolor='rgb(18, 20, 26)' # Fundo escuro estilo dashboard profissional
+    ),
 )
