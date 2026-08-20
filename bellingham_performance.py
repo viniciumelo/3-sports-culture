19fd304baa78d4d8df9f0385c70cac83de532ad8
Vinicius Melo <viniciumelo@hotmail.com>
2026-08-20 18:47:10 -0300

    - algoritmo de analise Bellingham

--- bellingham_performance.py
+++ bellingham_performance.py
@@ -32,5 +32,10 @@ fig.add_trace(go.Scatterpolar(
     r=valores_grafico_fechados,
     theta=metricas_fechadas,
     fill='toself',
+    fillcolor='rgba(255, 215, 0, 0.3)',  # Tom dourado
+    line=dict(color='gold', width=2),
+    text=valores_reais_fechados,
+    hovertemplate="<b>%{theta}</b><br>Nível Geral: %{r}/100<br>Dado Real: %{text}<extra></extra>",
+    name='Jude Bellingham'
 ))
 
