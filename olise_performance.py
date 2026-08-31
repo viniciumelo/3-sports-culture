ac24091b629bd79220f0ffeef41f90662b96e5ea
Vinicius Melo <viniciumelo@hotmail.com>
2026-08-30 23:03:28 -0300

    - algoritmo de analise Olise

---
+++ olise_performance.py
@@ -0,0 +1,8 @@
+import plotly.graph_objects as go
+
+# 1. Definição das métricas e os valores médios de desempenho do Michael Olise
+# Os valores do gráfico estão em escala de 0 a 100 para manter a proporção visual ideal,
+# enquanto os dados de scout especializado aparecem no detalhamento (hover).
+metricas = [
+    'Posse de Bola<br>(Dribles e Retenção)', 
+]
