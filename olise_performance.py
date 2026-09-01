87511821aaecebfce5586c8599d5735953d1026e
Vinicius Melo <viniciumelo@hotmail.com>
2026-08-31 23:01:54 -0300

    - algoritmo de analise Olise

--- olise_performance.py
+++ olise_performance.py
@@ -16,4 +16,5 @@ valores_grafico = [88, 85, 93]
 # Dados reais detalhados que aparecem ao passar o mouse (hover)
 valores_reais = [
     "Alta retenção (Média de 4.8 dribles/jogo e condução segura)", 
+    "33.6 km/h de pico (Aceleração rápida na ponta)", 
 ]
