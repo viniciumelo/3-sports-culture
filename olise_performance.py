9f95aef0768674b3fb38bf604aafb5d35b5f71fb
Vinicius Melo <viniciumelo@hotmail.com>
2026-09-01 23:40:36 -0300

    - algoritmo de analise Olise

--- olise_performance.py
+++ olise_performance.py
@@ -32,4 +32,9 @@ fig.add_trace(go.Scatterpolar(
     r=valores_grafico_fechados,
     theta=metricas_fechadas,
     fill='toself',
+    fillcolor='rgba(220, 20, 60, 0.3)',  # Tom vermelho/carmesim
+    line=dict(color='crimson', width=2),
+    text=valores_reais_fechados,
+    hovertemplate="<b>%{theta}</b><br>Nível Geral: %{r}/100<br>Dado Real: %{text}<extra></extra>",
+    name='Michael Olise'
 ))
