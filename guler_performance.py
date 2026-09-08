d91b68234ce6288c0c9fabeb09c8833031ae3475
Vinicius Melo <viniciumelo@hotmail.com>
2026-09-08 20:48:29 -0300

    - algoritmo de analise Guler

--- guler_performance.py
+++ guler_performance.py
@@ -5,4 +5,5 @@ import plotly.graph_objects as go
 # enquanto os dados de scout especializado aparecem no detalhamento (hover).
 metricas = [
     'Posse de Bola<br>(Dribles, Controle e Passe)', 
+    'Velocidade Máxima<br>(Agilidade e Aceleração)', 
 ]
